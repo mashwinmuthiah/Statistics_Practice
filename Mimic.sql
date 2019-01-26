b3316c6e4767dce12f791fec201ab7c64d4eda5f
mashwinmuthiah <ashwin.muthiah1996@gmail.com>
2019-01-26 10:12:55 -0500

    Mimic

---
+++ Mimic.sql
@@ -0,0 +1,72 @@
+SELECT p.subject_id, p.dob, a.hadm_id,
+    a.admittime, p.expire_flag,
+    MIN (a.admittime) OVER (PARTITION BY p.subject_id) AS first_admittime
+FROM admissions a
+INNER JOIN patients p
+ON p.subject_id = a.subject_id
+ORDER BY a.hadm_id, p.subject_id;
+
+WITH first_admission_time AS
+(
+  SELECT
+      p.subject_id, p.dob, p.gender
+      , MIN (a.admittime) AS first_admittime
+      , MIN( ROUND( (cast(admittime as date) - cast(dob as date)) / 365.242,2) )
+          AS first_admit_age
+  FROM patients p
+  INNER JOIN admissions a
+  ON p.subject_id = a.subject_id
+  GROUP BY p.subject_id, p.dob, p.gender
+  ORDER BY p.subject_id
+)
+SELECT
+    subject_id, dob, gender
+    , first_admittime, first_admit_age
+    , CASE
+        -- all ages > 89 in the database were replaced with 300
+        WHEN first_admit_age > 89
+            then '>89'
+        WHEN first_admit_age >= 14
+            THEN 'adult'
+        WHEN first_admit_age <= 1
+            THEN 'neonate'
+        ELSE 'middle'
+        END AS age_group
+FROM first_admission_time
+ORDER BY subject_id
+
+WITH first_admission_time AS
+(
+  SELECT
+      p.subject_id, p.dob, p.gender
+      , MIN (a.admittime) AS first_admittime
+      , MIN( ROUND( (cast(admittime as date) - cast(dob as date)) / 365.242,2) )
+          AS first_admit_age
+  FROM patients p
+  INNER JOIN admissions a
+  ON p.subject_id = a.subject_id
+  GROUP BY p.subject_id, p.dob, p.gender
+  ORDER BY p.subject_id
+)
+, age as
+(
+  SELECT
+      subject_id, dob, gender
+      , first_admittime, first_admit_age
+      , CASE
+          -- all ages > 89 in the database were replaced with 300
+          -- we check using > 100 as a conservative threshold to ensure we capture all these patients
+          WHEN first_admit_age > 100
+              then '>89'
+          WHEN first_admit_age >= 14
+              THEN 'adult'
+          WHEN first_admit_age <= 1
+              THEN 'neonate'
+          ELSE 'middle'
+          END AS age_group
+  FROM first_admission_time
+)
+select age_group, gender
+  , count(subject_id) as NumberOfPatients
+from age
+group by age_group, gender
